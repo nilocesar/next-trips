a3b8d7578626b067c26e2fdbb3f56797b64c4213
Nilo César <e_maildonilo@yahoo.com.br>
2021-03-18 09:52:36 -0300

    Initial commit from Create Next App

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+    // eslint-disable-next-line prettier/prettier
+    pwa: {
+        dest: 'public',
+        disable: !isProd
+    }
+})
